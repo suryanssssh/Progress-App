d234d099b8b4bbf7cb99db5b456ab99623179dc4
suryanshsinghnasahicker <suryanshsingh983810@gmail.com>
2024-01-24 09:44:54 +0530

    done with authentication

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 function App() {
   return (
     <Router>
--- my-react-app/src/pages/Home.js
+++ my-react-app/src/pages/Home.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { signOut } from "firebase/auth";
+import { auth } from "../db/config/firebase";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        navigate("/");
+        console.log("Signed out successfully");
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
+
+  return (
+    <>
+      <nav>
+        <p>Welcome Home</p>
+
+        <div>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Home;
